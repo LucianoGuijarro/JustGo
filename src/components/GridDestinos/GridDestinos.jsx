998ed356bed4c0d2906577eca7971aa4c394f324
Luciano Ezequiel Guijarro <guijarro.luciano1995@gmail.com>
2022-10-09 14:53:59 +0200

    agrego funcionabilidad de carrusel de cards en los destinos

--- src/components/GridDestinos/GridDestinos.jsx
+++ src/components/GridDestinos/GridDestinos.jsx
@@ -1,44 +1,56 @@
-import styles from './GridDestinos.module.css'
-import imagenBerlin from '../../img/Berlin.jpg'
+import styles from './GridDestinos.module.css';
+import imagenBerlin from '../../img/Berlin.jpg';
+import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
 
 const GridDestinos = () => {
-  return (
-    <>
-    <h2 className='fs-1 fw-bold m-5 text-center'>Destinos Working Holidays</h2>
-    <div className="container my-5">
-        <div className={`${styles.grid} py-4 row justify-content-beetween`}>
-            <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
-                <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
-                <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
+
+    const sliderRigth = () => {
+        let left = document.getElementById('slider')
+        left.scrollLeft = left.scrollLeft + 200;
+    }
+    const sliderLeft = () => {
+        let left = document.getElementById('slider')
+        left.scrollLeft = left.scrollLeft - 200;
+    }
+    return (
+        <>
+            <h2 className='fs-1 fw-bold m-5 text-center'>Destinos Working Holidays</h2>
+            <div className="container my-5 d-flex flex-nowrap align-items-center">
+                <BsArrowLeftCircle className={`${styles.left} mx-4`} onClick={sliderLeft} />
+                <div className={`${styles.grid} py-4 row justify-content-beetween align-items-center`} id='slider'>
+                    <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                        <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
+                        <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
+                    </div>
+                    <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                        <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
+                        <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
+                    </div>
+                    <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                        <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
+                        <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
+                    </div>
+                    <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                        <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
+                        <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
+                    </div>
+                    <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                        <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
+                        <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
+                    </div>
+                    <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                        <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
+                        <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
+                    </div>
+                    <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                        <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
+                        <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
+                    </div>
+                </div>
+                <BsArrowRightCircle className={`${styles.right} mx-4`} onClick={sliderRigth} />
             </div>
-            <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
-                <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
-                <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
-            </div>
-            <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
-                <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
-                <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
-            </div>
-            <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
-                <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
-                <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
-            </div>
-            <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
-                <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
-                <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
-            </div>
-            <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
-                <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
-                <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
-            </div>
-            <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
-                <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
-                <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
-            </div>
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default GridDestinos
