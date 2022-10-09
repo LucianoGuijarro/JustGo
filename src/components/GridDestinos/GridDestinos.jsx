cfdac0da3612e135ed1701f80cb20d3034916edb
Luciano Ezequiel Guijarro <guijarro.luciano1995@gmail.com>
2022-10-09 17:57:38 +0200

    agrego funcionabilidad al carrusel destinos

--- src/components/GridDestinos/GridDestinos.jsx
+++ src/components/GridDestinos/GridDestinos.jsx
@@ -6,43 +6,51 @@ const GridDestinos = () => {
 
     const sliderRigth = () => {
         let left = document.getElementById('slider')
-        left.scrollLeft = left.scrollLeft + 200;
+        left.scrollLeft = left.scrollLeft + 230;
     }
     const sliderLeft = () => {
         let left = document.getElementById('slider')
-        left.scrollLeft = left.scrollLeft - 200;
+        left.scrollLeft = left.scrollLeft - 230;
     }
     return (
         <>
             <h2 className='fs-1 fw-bold m-5 text-center'>Destinos Working Holidays</h2>
             <div className="container my-5 d-flex flex-nowrap align-items-center">
                 <BsArrowLeftCircle className={`${styles.left} mx-4`} onClick={sliderLeft} />
-                <div className={`${styles.grid} py-4 row justify-content-beetween align-items-center`} id='slider'>
-                    <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                <div className={`${styles.grid} py-4 row justify-content-start align-items-center`} id='slider'>
+                    <div className={`${styles.cardDestinos} col-7 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
                         <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
                         <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
                     </div>
-                    <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                    <div className={`${styles.cardDestinos} col-7 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
                         <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
                         <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
                     </div>
-                    <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                    <div className={`${styles.cardDestinos} col-7 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
                         <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
                         <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
                     </div>
-                    <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                    <div className={`${styles.cardDestinos} col-7 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
                         <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
                         <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
                     </div>
-                    <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                    <div className={`${styles.cardDestinos} col-7 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
                         <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
                         <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
                     </div>
-                    <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                    <div className={`${styles.cardDestinos} col-7 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
                         <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
                         <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
                     </div>
-                    <div className={`${styles.cardDestinos} col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                    <div className={`${styles.cardDestinos} col-7 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                        <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
+                        <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
+                    </div>
+                    <div className={`${styles.cardDestinos} col-7 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
+                        <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
+                        <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
+                    </div>
+                    <div className={`${styles.cardDestinos} col-7 col-sm-5 col-md-5 col-lg-2 col-xl-2 shadow border p-0`}>
                         <img className={`${styles.imagen} img-fluid`} src={imagenBerlin} alt="Berlin" />
                         <p className='text-center pt-3 fs-4 fw-bold'>Alemania</p>
                     </div>
